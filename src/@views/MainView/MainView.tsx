6763caf129b9e1531c975debd3c27a6e1edc9dc3
Alexandr Siryj <theobroma222@gmail.com>
2022-04-21 14:31:28 +0300

    forecast days

--- src/@views/MainView/MainView.tsx
+++ src/@views/MainView/MainView.tsx
@@ -2,6 +2,7 @@ import { Container, Grid, Paper } from '@mui/material';
 import Box from '@mui/material/Box';
 import React, { useEffect } from 'react';
 import CurrentWeather from '../../@components/CurrentWeather';
+import Forecast from '../../@components/Forecast';
 import { useAppDispatch, useAppSelector } from '../../@store/configureStore';
 import { getForecastTC } from '../../@store/forecast/slice';
 import { сoordinatesSelector } from '../../@store/сoordinates/selectors';
@@ -51,8 +52,8 @@ const MainView = () => {
           <Grid item xs={12}>
             <Box mb={1}>
               <Paper elevation={3}>
-                {/* <Forecast /> */}
-                Forecast
+                {/* Forecast */}
+                <Forecast />
               </Paper>
             </Box>
           </Grid>
