70d0d2439de3ace85253eb3b6bc8c0989012c059
Alexandr Siryj <theobroma222@gmail.com>
2022-04-23 00:58:32 +0300

    light and dark theme

--- src/@themes/theme.tsx
+++ src/@themes/theme.tsx
@@ -1,6 +1,6 @@
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
-import { red } from '@mui/material/colors';
+import { PaletteMode } from '@mui/material';
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import React from 'react';
@@ -11,21 +11,46 @@ export const muiCache = createCache({
 });
 
 // A custom theme for this app
-const theme = createTheme({
+const LIGHT_THEME = {
   palette: {
+    mode: 'light' as PaletteMode,
+    background: {
+      paper: '#f5f5f5', // Gray 100 - Background of "Paper" based component
+      default: '#FFFFFF',
+    },
     primary: {
       main: '#556cd6',
+      contrastText: '#fff',
     },
     secondary: {
       main: '#81c784',
-    },
-    error: {
-      main: red.A400,
+      contrastText: '#000',
     },
   },
-});
+};
+
+const DARK_THEME = {
+  palette: {
+    mode: 'dark' as PaletteMode,
+    background: {
+      paper: '#424242', // Gray 800 - Background of "Paper" based component
+      default: '#303030',
+    },
+    primary: {
+      main: '#81c784', // Green 300
+      contrastText: '#000000',
+    },
+    secondary: {
+      main: '#ffb74d', // Orange 300
+      contrastText: '#000',
+    },
+  },
+};
 
 const AppThemeProvider = ({ children }: { children?: React.ReactNode }) => {
+  // const currentTheme = useSelector(themeSelector);
+  // const theme = createTheme(themeMap[currentTheme]);
+  const theme = createTheme(LIGHT_THEME);
   return (
     <CacheProvider value={muiCache}>
       <ThemeProvider theme={theme}>
