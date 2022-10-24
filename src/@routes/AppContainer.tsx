c008ab11b7d3a3dfd86d786b91f08331cc1a1c17
Alexandr Siryj <theobroma222@gmail.com>
2022-10-24 04:06:22 +0300

    browser router v 6.4

--- src/@routes/AppContainer.tsx
+++ src/@routes/AppContainer.tsx
@@ -1,21 +1,27 @@
 // https://stackoverflow.com/questions/69928061/struggling-with-typescript-react-eslint-and-simple-arrow-functions-components
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+// https://github.com/michey85/youtube-react-router-v6/blob/router-v6.4/src/App.jsx
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
 
 import MainView from '../@views/MainView';
 
 import { AppLayout } from './AppLayout';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<AppLayout />}>
+      <Route index element={<MainView />} />
+      {/* <Route path="cryptocurrencies" element={<MainView />} /> */}
+      {/* <Route path="exchanges" element={<div>exchanges</div>} /> */}
+      <Route path="*" element={<div>Not Found</div>} />
+    </Route>,
+  ),
+);
+
 export const AppContainer = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<MainView />} />
-          {/* <Route path="cryptocurrencies" element={<MainView />} /> */}
-          {/* <Route path="exchanges" element={<div>exchanges</div>} /> */}
-          <Route path="*" element={<div>Not Found</div>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 };
